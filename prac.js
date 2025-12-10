9b314a7fe625bbefb7fda8241cb1d33a146f40e0
shaikhalinawaz140-sudo <shaikhalinawaz140@gmail.com>
2025-12-10 10:30:27 +0500

    updated changes

---
+++ prac.js
@@ -0,0 +1,77 @@
+// // var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// // for (var i = i; i < 10; i++) {
+// //     console.log(i)
+// // vae arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, ]
+// // for () {
+
+// // }
+// // assigment 1 to 10 chepter1,
+// // Q1
+// //////;alert hello world
+// // Q3
+// // namvarvar;
+// // var chehstudendt = prompt("enter the name")
+// // var student = ["ali", "ibrahim", "shabby", "aniq"];
+
+// // var name = [1, 2, 3, 4, 5, 6, 7, 7, 8, 8]
+// // for (var i = 0; i < 10; i++) {
+// //     console.log(i)
+// // 
+// // // var number=(1,2,3,4,5)
+// // for(i=1; i <=5; i++) {
+// //     console.log(number)
+// // }
+// // // for loop 1
+// // for ( let i=1; i<=10; i++){
+// //     console.log(i)
+// // }
+// // Backward Loop (Reverse Loop)
+
+// // for(let i=10; i >=1; i-- ){
+// //     console.log(i)
+// }
+// // Step (2–2 ya 5–5 barhana)
+
+// // for(let i=1; i<=20; i+=2){
+// //     console.log(i)
+// // }
+// // nested loop
+// // var num=prompt("enter number")
+// // for (let i = 1; i <= 10; i++) {
+//     console.log(`i x ${i} = ${2 * i/}`);
+// // 
+// for (let i = 1; i <= 10; i++){
+//     console.log(`i x ${i} = ${2 * i}`);
+// }
+// ARRY
+// let fruits = ["Apple", "Banana", "Mango"];
+
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i]);
+// }
+
+
+// var cityName="world war II"
+// var cityName=cityName.length;
+
+//  for(i
+//     i
+// //  )
+// var name="ibr"
+// var check=name(ibr)
+// if(name.indexof("ibr")){
+//     console.log(milgya)
+// }
+
+
+
+// var student="this ia a good student and student";
+// var result=student.replace(/student/g,"ali");
+// console.log(result);
+// for(let i=1; i<=10; i++){
+    // console.log(i);
+// }
+
+var die=Math.round(Math.random()*6)+1;{
+console.log(die)
+}
